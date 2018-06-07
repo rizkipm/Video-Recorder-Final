7ad0930bc660c3b2d50c5e0e26d3603afe42f3eb
Rizki Syaputra <rizki@oddbit.id>
2018-06-08 01:11:03 +0800

    add

--- app/src/main/java/superheroku/id/co/finalvideorecorder/MainActivity.java
+++ app/src/main/java/superheroku/id/co/finalvideorecorder/MainActivity.java
@@ -372,15 +372,22 @@ public class MainActivity extends AppCompatActivity implements MediaRecorder.OnI
                 closeCamera();
                 if (mCameraId.equalsIgnoreCase("0")) {
                     mCameraId = "1";
+//                    stopRecordingWhileSwitchingVideo(false);
+//                    closeCamera();
+//                    reopenCamera();
 //                    mMediaRecorder.setVideoSize(mTextureView.getWidth(), mTextureView.getHeight());
 //                    mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
                 } else {
                     mCameraId = "0";
+//                    stopRecordingWhileSwitchingVideo(false);
+//                    closeCamera();
+//                    reopenCamera();
 //                    mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
                 }
                 connectCamera();
+//                stopRecordingWhileSwitchingVideo(false);
 
-                stopRecordingWhileSwitchingVideo(true);
+
 
             }
         });
@@ -432,6 +439,31 @@ public class MainActivity extends AppCompatActivity implements MediaRecorder.OnI
         });
     }
 
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+
     static class CompareSizesByArea implements Comparator<Size> {
 
         @SuppressLint("NewApi")
@@ -461,14 +493,41 @@ public class MainActivity extends AppCompatActivity implements MediaRecorder.OnI
         // UI
 
         mIsRecording = false;
+        if (start){
+            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
+        }
+
+
+//        connectCamera();
+
+
+//        if (mTextureView.isAvailable()) {
+//            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
+//        } else {
+//            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+//        }
 //        mMediaRecorder.stop();
 //        mMediaRecorder.reset();
 
 //        closeCamera();
 
-        if (start)
-            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
+//        if (start){
+//            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
+//        }
+
+
+
+
     }
+
+    private void reopenCamera() {
+        if (mTextureView.isAvailable()) {
+            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
+        } else {
+            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
+    }
+
 
     @Override
     public void onWindowFocusChanged(boolean hasFocas) {
